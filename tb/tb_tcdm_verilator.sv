58f01493df861259b38d1e2d8b18a46e4550fb39
daro <daro@tsr>
2024-12-12 16:57:41 +0200

    refacored multiport memory to support delays, work in progress

--- tb/tb_tcdm_verilator.sv
+++ tb/tb_tcdm_verilator.sv
@@ -7,7 +7,7 @@ module tb_tcdm_verilator #(
     parameter MP = 1,  // Number of memory ports (default 1)
     parameter MEMORY_SIZE = 1024,  // Size of the memory (default 1024 entries)
     parameter BASE_ADDR = 0,  // Base address for memory access (default 0)
-    parameter RD_DELAY_CYCLES = 0  // Number of clock cycles to delay for read operations (default 2)
+    parameter RD_DELAY_CYCLES = 2  // Number of clock cycles to delay for read operations (default 2)
 
 ) (
     input logic                       clk_i,
@@ -16,17 +16,34 @@ module tb_tcdm_verilator #(
           hwpe_stream_intf_tcdm.slave tcdm    [MP-1:0]
 );
 
-  logic [   7:0]       memory                                                  [MEMORY_SIZE];
+  logic [   7:0]       memory         [MEMORY_SIZE];
 
   logic [MP-1:0][31:0] write_data;
 
   logic [MP-1:0][31:0] tcdm_r_data_d;
   logic [MP-1:0]       tcdm_r_valid_d;
+  logic [MP-1:0]       tcdm_gnt;
   logic [MP-1:0][ 1:0] misalignment;
-  logic [  31:0]       index                                                   [     MP-1:0];
+  logic [  31:0]       index          [     MP-1:0];
 
   int                  cnt_wr = 0;
   int                  cnt_rd = 0;
+
+  typedef enum logic [2:0] {
+    STALL,
+    GRANT,
+    GRANTING
+  } gnt_state_t;
+
+  typedef enum logic [2:0] {
+    IDLE,
+    READ,
+    WRITE
+  } mem_state_t;
+
+  gnt_state_t [MP-1:0] gnt_state;
+  gnt_state_t [MP-1:0] gnt_next;
+  mem_state_t [MP-1:0] mem_state;
 
   // Programmable delay counters for each read port
   logic [MP-1:0][31:0] rd_delay_counter;  // Delay counter for each memory port
@@ -35,10 +52,48 @@ module tb_tcdm_verilator #(
   // Generate block for each memory port
   generate
     for (genvar i = 0; i < MP; i++) begin : gen_mem_proc
-      assign tcdm[i].gnt = tcdm[i].req;  // Always grant access for simplicity
-      assign tcdm[i].r_data = tcdm_r_data_d[i];
+      //assign tcdm[i].gnt = tcdm[i].req;  // Always grant access for simplicity
+      //assign tcdm[i].gnt = tcdm_gnt[i] & tcdm[i].req;
+      assign tcdm[i].r_data  = tcdm_r_data_d[i];
       assign tcdm[i].r_valid = tcdm_r_valid_d[i];
       assign misalignment[i] = tcdm[i].add[1:0];  // Get the last 2 bits of the address
+
+
+      always_comb begin
+        case (gnt_state[i])
+          STALL: gnt_next[i] = (rd_delay_counter[i] == 0) ? GRANT : STALL;
+          GRANT: gnt_next[i] = STALL;
+          // GRANTING: gnt_next[i] = STALL;
+
+        endcase
+
+      end
+
+      // Always block to process read and write operations
+      always_ff @(posedge clk_i or negedge rst_ni) begin
+        if (~rst_ni) begin
+          tcdm_gnt[i] <= 1'b0;
+          rd_delay_counter[i] <= RD_DELAY_CYCLES;
+          gnt_state[i] = STALL;
+
+        end else begin
+          gnt_state[i] <= gnt_next[i];
+          case (gnt_next[i])
+            GRANT: begin
+              tcdm_gnt[i] <= tcdm[i].req;
+              rd_delay_counter[i] <= RD_DELAY_CYCLES;
+            end
+            STALL: begin
+              tcdm_gnt[i] <= 1'b0;
+              if (tcdm[i].req) begin
+                rd_delay_counter[i] <= rd_delay_counter[i] - 1;
+              end else begin
+                rd_delay_counter[i] <= RD_DELAY_CYCLES;
+              end
+            end
+          endcase
+        end
+      end
 
       always_comb begin
         case (misalignment[i])
@@ -69,39 +124,42 @@ module tb_tcdm_verilator #(
         if (~rst_ni) begin
           tcdm_r_data_d[i] <= '0;
           tcdm_r_valid_d[i] <= '0;
-          rd_delay_counter[i] <= RD_DELAY_CYCLES;
-          rd_busy <=0;
+          mem_state[i] <= IDLE;
         end else begin
-          if (tcdm[i].req & ~tcdm[i].wen) begin  // Write
+          if (tcdm_gnt[i] & tcdm[i].req & ~tcdm[i].wen) begin  // Write
+            mem_state[i] <= WRITE;
             cnt_wr += 1;
-            memory[index[i]]   <= write_data[i][7:0];
+            memory[index[i]] <= write_data[i][7:0];
             memory[index[i]+1] <= write_data[i][15:8];
             memory[index[i]+2] <= write_data[i][23:16];
             memory[index[i]+3] <= write_data[i][31:24];
             //
-            tcdm_r_data_d[i]   <= write_data[i];
-            tcdm_r_valid_d[i]  <= 1'b1;
+            tcdm_r_data_d[i] <= write_data[i];
+            tcdm_r_valid_d[i] <= 1'b1;
+            tcdm[i].gnt <= 1;
           end else begin
-            if (tcdm[i].req & tcdm[i].wen) begin  // Read
-              if(~rd_busy) begin
-                rd_busy<=1;
-              tcdm_r_data_d[i][7:0]   <= memory[index[i]];
-              tcdm_r_data_d[i][15:8]  <= memory[index[i]+1];
+            if (tcdm_gnt[i] & tcdm[i].req & tcdm[i].wen) begin  //READ
+              mem_state[i] <= READ;
+              tcdm_r_data_d[i][7:0] <= memory[index[i]];
+              tcdm_r_data_d[i][15:8] <= memory[index[i]+1];
               tcdm_r_data_d[i][23:16] <= memory[index[i]+2];
               tcdm_r_data_d[i][31:24] <= memory[index[i]+3];
-              end
-              if (rd_delay_counter[i] == 0) begin
-                tcdm_r_valid_d[i] <= 1'b1;
-                cnt_rd += 1;
-                rd_delay_counter[i] = RD_DELAY_CYCLES;
-                rd_busy<=0;
-              end else begin
-                rd_delay_counter[i] = rd_delay_counter[i] - 1;
-                tcdm_r_valid_d[i] <= 1'b0;
-              end
+
+              tcdm_r_valid_d[i] <= 1'b1;
+              tcdm[i].gnt <= 1;
+              cnt_rd += 1;
+
             end else begin
-              tcdm_r_data_d[i]  <= '0;
-              tcdm_r_valid_d[i] <= 1'b0;
+
+              tcdm[i].gnt <= 0;
+              case (mem_state[i])
+                READ, IDLE: begin
+                  tcdm_r_valid_d[i] <= 0;
+                  tcdm_r_data_d[i]  <= '0;
+                end
+                WRITE: mem_state[i] <= IDLE;
+
+              endcase
             end
 
           end
