05fded3d462a85aab68476c34025e16dc146be74
daro <daro@tsr>
2024-12-16 13:52:51 +0200

    WIP

--- tb/tb_tcdm_verilator.sv
+++ tb/tb_tcdm_verilator.sv
@@ -15,24 +15,22 @@ module tb_tcdm_verilator #(
           hwpe_stream_intf_tcdm.slave tcdm    [MP-1:0]
 );
 
-  logic [   7:0]       memory         [MEMORY_SIZE];
+  logic [   7:0]      memory       [MEMORY_SIZE];
 
-  logic [MP-1:0][31:0] write_data;
 
-  logic [MP-1:0][31:0] tcdm_r_data_d;
-  logic [MP-1:0]       tcdm_r_valid_d;
-  logic [MP-1:0][ 1:0] misalignment;
-  logic [  31:0]       index          [     MP-1:0];
 
-  int cnt_wr=0;
-  int cnt_rd=0;
+
+
+  logic [MP-1:0][1:0] misalignment;
+  logic [  31:0]      index        [     MP-1:0];
+
+  int                 cnt_wr = 0;
+  int                 cnt_rd = 0;
 
   // Generate block for each memory port
   generate
     for (genvar i = 0; i < MP; i++) begin : gen_mem_proc
       assign tcdm[i].gnt = tcdm[i].req;  // Always grant access for simplicity
-      assign tcdm[i].r_data = tcdm_r_data_d[i];
-      assign tcdm[i].r_valid = tcdm_r_valid_d[i];
       assign misalignment[i] = tcdm[i].add[1:0];  // Get the last 2 bits of the address
 
       always_comb begin
@@ -53,39 +51,36 @@ module tb_tcdm_verilator #(
             index[i] = (tcdm[i].add - BASE_ADDR) - 3;
           end
         endcase
-        write_data[i][7:0]   = tcdm[i].be[0] ? tcdm[i].data[7:0] : memory[index[i]];
-        write_data[i][15:8]  = tcdm[i].be[1] ? tcdm[i].data[15:8] : memory[index[i]+1];
-        write_data[i][23:16] = tcdm[i].be[2] ? tcdm[i].data[23:16] : memory[index[i]+2];
-        write_data[i][31:24] = tcdm[i].be[3] ? tcdm[i].data[31:24] : memory[index[i]+3];
+
       end
 
       // Always block to process read and write operations
       always_ff @(posedge clk_i or negedge rst_ni) begin
         if (~rst_ni) begin
-          tcdm_r_data_d[i]  <= '0;
-          tcdm_r_valid_d[i] <= '0;
+          tcdm[i].r_data  <= '0;
+          tcdm[i].r_valid <= '0;
         end else begin
           if (tcdm[i].req & ~tcdm[i].wen) begin  // Write
-            cnt_wr+=1;
-            memory[index[i]]   <= write_data[i][7:0];
-            memory[index[i]+1] <= write_data[i][15:8];
-            memory[index[i]+2] <= write_data[i][23:16];
-            memory[index[i]+3] <= write_data[i][31:24];
-            //
-            tcdm_r_data_d[i]   <= write_data[i];
-            tcdm_r_valid_d[i]  <= 1'b1;
+            cnt_wr += 1;
+            if (tcdm[i].be[0]) memory[index[i]] <= tcdm[i].data[7:0];
+            if (tcdm[i].be[1]) memory[index[i]+1] <= tcdm[i].data[15:8];
+            if (tcdm[i].be[2]) memory[index[i]+2] <= tcdm[i].data[23:16];
+            if (tcdm[i].be[3]) memory[index[i]+3] <= tcdm[i].data[31:24];
+            //loop back
+            tcdm[i].r_data  <= tcdm[i].data;
+            tcdm[i].r_valid <= 1'b1;
           end else begin
             if (tcdm[i].req & tcdm[i].wen) begin  // Read
-              cnt_rd+=1;
-              tcdm_r_data_d[i][7:0] <= memory[index[i]];
-              tcdm_r_data_d[i][15:8] <= memory[index[i]+1];
-              tcdm_r_data_d[i][23:16] <= memory[index[i]+2];
-              tcdm_r_data_d[i][31:24] <= memory[index[i]+3];
+              cnt_rd += 1;
+              tcdm[i].r_data[7:0] <= memory[index[i]];
+              tcdm[i].r_data[15:8] <= memory[index[i]+1];
+              tcdm[i].r_data[23:16] <= memory[index[i]+2];
+              tcdm[i].r_data[31:24] <= memory[index[i]+3];
 
-              tcdm_r_valid_d[i] <= 1'b1;
+              tcdm[i].r_valid <= 1'b1;
             end else begin
-              tcdm_r_data_d[i]  <= '0;
-              tcdm_r_valid_d[i] <= 1'b0;
+              tcdm[i].r_data  <= '0;
+              tcdm[i].r_valid <= 1'b0;
             end
 
           end
